0619bba18733eaf3491007159351a6aa935e6959
Cristhoffer Santos <nunes-santos@hotmail.com>
2024-03-08 20:54:14 -0300

    feat: add sign up to common user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, ValidationPipe, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  async signUp(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ): Promise<{ message: string }> {
+    console.log(createUserDto);
+    await this.authService.signUp(createUserDto);
+    return {
+      message: 'Cadastro realizado com sucesso',
+    };
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UserRole } from 'src/users/enum/user-roles.enum';
+import { User } from 'src/users/user.entity';
+import { UserRepository } from 'src/users/users.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: UserRepository,
+  ) {}
+
+  async signUp(createUserDto: CreateUserDto): Promise<User> {
+    if (createUserDto.password != createUserDto.passwordConfirmation) {
+      throw new UnprocessableEntityException('As senhas não conferem');
+    } else {
+      return await this.userRepository.createUser(createUserDto, UserRole.USER);
+    }
+  }
+}
