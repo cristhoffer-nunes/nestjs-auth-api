bf05da0e93e70b9c70b20042773f03820da7e1b1
Cristhoffer Santos <nunes-santos@hotmail.com>
2024-03-08 23:03:34 -0300

    feat: allow creation of admin user only with jwt admin token

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,13 +9,17 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ReturnUserDto } from './dto/return-user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { UserRole } from './enum/user-roles.enum';
+import { RolesGuard } from 'src/auth/guards/roles.guards';
+import { Role } from 'src/auth/decorators/role.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  @UseGuards(AuthGuard())
+  @Role(UserRole.ADMIN)
+  @UseGuards(AuthGuard(), RolesGuard)
   async createAdminUser(
     @Body(ValidationPipe) createUserDto: CreateUserDto,
   ): Promise<ReturnUserDto> {
